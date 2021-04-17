307d47d6d12013a811898392df79c48a68335528
mauro-773 <mauromamani.b@gmail.com>
2021-04-17 15:56:57 -0300

    renderizando imagenes del markdown con el componente Image de next

--- components/postsPage/postDetail/PostContent.js
+++ components/postsPage/postDetail/PostContent.js
@@ -1,14 +1,49 @@
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
 import PostHeader from './PostHeader';
 import classes from './PostContent.module.css';
 
 const PostContent = ({ post }) => {
    const imagePath = `/images/posts/${post.slug}/${post.image}`;
+   // customComponents: Cambiamos los elementos del markdown antes de pasarlos a JSX
+   const customComponents = {
+      p: (paragraph) => {
+         const { node } = paragraph;
+         if (node.children[0].tagName === 'img') {
+            const image = node.children[0];
+            return (
+               <div className={classes.image}>
+                  <Image
+                     src={`/images/posts/${post.slug}/${image.properties.src}`}
+                     alt={image.properties.alt}
+                     width={600}
+                     height={300}
+                  />
+               </div>
+            );
+         }
+         return <p>{paragraph.children}</p>;
+      },
+      code: (code) => {
+         return (
+            <SyntaxHighlighter
+               style={atomDark}
+               children={code.children[0]}
+               language="js"
+            />
+         );
+      },
+   };
 
    return (
       <article className={classes.content}>
          <PostHeader title={post.title} image={imagePath} />
-         <ReactMarkdown>{post.content}</ReactMarkdown>
+         <ReactMarkdown components={customComponents}>
+            {post.content}
+         </ReactMarkdown>
       </article>
    );
 };
