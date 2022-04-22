a014fb73c12c95589d5a34b13c8901d89e53d707
mauromamani <mauromamani.b@gmail.com>
2022-04-22 15:34:24 -0300

    chore: add mongo atlas

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,44 +1,46 @@
 import { MongoClient } from 'mongodb';
 
+const MONGO_URI = process.env.MONGO_URI;
+
 const handler = async (req, res) => {
-   if (req.method === 'POST') {
-      try {
-         const { email, name, message } = req.body;
+  if (req.method === 'POST') {
+    try {
+      const { email, name, message } = req.body;
 
-         const client = await MongoClient.connect(
-            'mongodb://127.0.0.1:27017/events-app'
-         );
+      const client = await MongoClient.connect(MONGO_URI, {
+        useUnifiedTopology: true,
+      });
 
-         if (
-            !email.trim() ||
-            !email.includes('@') ||
-            !name.trim() ||
-            !message.trim()
-         ) {
-            return res.status(422).json({ ok: false });
-         }
-
-         const db = client.db();
-         const result = await db
-            .collection('messages')
-            .insertOne({ email, name, message });
-
-         client.close();
-
-         res.status(201).json({
-            ok: true,
-            message: 'Succesfully stored message!',
-            newMessage: {
-               _id: result.insertedId,
-               name,
-               message,
-               email,
-            },
-         });
-      } catch (error) {
-         console.log(error);
+      if (
+        !email.trim() ||
+        !email.includes('@') ||
+        !name.trim() ||
+        !message.trim()
+      ) {
+        return res.status(422).json({ ok: false });
       }
-   }
+
+      const db = client.db();
+      const result = await db
+        .collection('messages')
+        .insertOne({ email, name, message });
+
+      client.close();
+
+      res.status(201).json({
+        ok: true,
+        message: 'Succesfully stored message!',
+        newMessage: {
+          _id: result.insertedId,
+          name,
+          message,
+          email,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
 
 export default handler;
