377398a57b2c139377c830b00d3887d8647b46c0
mauromamani <mauromamani.b@gmail.com>
2022-04-22 16:48:42 -0300

    refactor: fallback false

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -4,36 +4,36 @@ import PostContent from '../../components/postsPage/postDetail/PostContent';
 import { getPostData, getPostsFiles } from '../../lib/postsUtil';
 
 const SingleProduct = ({ singlePost }) => {
-   return (
-      <>
-         <Head>
-            <title>{singlePost.title}</title>
-            <meta name="description" content={singlePost.excerpt} />
-         </Head>
-         <PostContent post={singlePost} />
-      </>
-   );
+  return (
+    <>
+      <Head>
+        <title>{singlePost.title}</title>
+        <meta name='description' content={singlePost.excerpt} />
+      </Head>
+      <PostContent post={singlePost} />
+    </>
+  );
 };
 
 export const getStaticProps = async (context) => {
-   const { slug } = context.params;
-   const singlePost = getPostData(slug);
+  const { slug } = context.params;
+  const singlePost = getPostData(slug);
 
-   return { props: { singlePost }, revalidate: 600 };
+  return { props: { singlePost }, revalidate: 600 };
 };
 
 export const getStaticPaths = async () => {
-   const postFilenames = getPostsFiles();
-   const slugs = postFilenames
-      .map((fileName) => fileName.replace(/\.md$/, ''))
-      .map((slug) => ({
-         params: { slug },
-      }));
+  const postFilenames = getPostsFiles();
+  const slugs = postFilenames
+    .map((fileName) => fileName.replace(/\.md$/, ''))
+    .map((slug) => ({
+      params: { slug },
+    }));
 
-   return {
-      paths: slugs,
-      fallback: true,
-   };
+  return {
+    paths: slugs,
+    fallback: false,
+  };
 };
 
 export default SingleProduct;
